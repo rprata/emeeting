17817592e93922e3654c79f51cc715756dd2fb4c
rfaria <renan.faria@totvs.com.br>
2016-07-21 14:12:24 -0300

    Adicionando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imeeting::Application.config.secret_key_base = '82268a727a2a6a764452f9e86dd1a342d52450117f15119bffeaea03f04a30826fc3675b891f8fe37e957d701199aba0e9980dd7551487c00feddf846738e0e1'
